8833980e132bd91fe0760f982fa6a270ef669b11
Gina <stahly.g894101@stu.sanjac.edu>
2021-12-07 17:21:38 -0600

    added new files

--- nintendo.php
+++ nintendo.php
@@ -1,13 +1,9 @@
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Home | BigBoss Game Store</title>
-    <link rel="stylesheet" href="assets/css/master.css">
-    <link rel="stylesheet" href="assets/css/slider.css">
-    <link rel="stylesheet" href="assets/css/cart.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<html>
+  <title>Nintendo Games | BigBoss Game Store</title>
+    <link rel="stylesheet" href="../assets/css/master.css">
+    <link rel="stylesheet" href="../assets/css/cart.css">
+    <link rel="stylesheet" href="../assets/css/slider.css">
     <link href="https://fonts.googleapis.com/css2?family=Teko:wght@300;400;500&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/732cfffd08.js" crossorigin="anonymous"></script>
     <script type="text/javascript">
@@ -16,51 +12,44 @@
         if(this.scrollY <= 10) nav.className = ''; else nav.className = 'scroll';
       };
     </script>
+
     <?php include 'session.php'; ?>
+
+    <style>
+				@import url('https://fonts.googleapis.com/css2?family=Open+Sans&family=Work+Sans:wght@300&display=swap');
+			</style>
   </head>
+
+
   <body>
-    <nav id="main-nav">
-        <a href="contact.php"><img id="logo" src="assets/images/logo.svg" alt="Logo" height="60"></a>
-      <div id="links">
-        <a href="games.html"><i class="fas fa-at"></i></a>
-        <a href="https://goo.gl/maps/5Gnu6bCmPon9rvB86"><i class="fas fa-map-marker-alt"></i></a>
-        <a href="checkout.php"><i class="fas fa-shopping-cart"></i></a>
-      </div>
-    </nav>
 
-    <nav class="nav-container">
-      <div id="content-nav">
-        <a href="index.php">Home</a>
-        <div class="dropdown">
-          <a class="dropbtn">Games <i class="fa fa-caret-down" style="font-size: 16px;"></i></a>
-          <div class="dropdown-content">
-            <a href="playstation.php">Playstation</a>
-            <a href="xbox.php">Xbox</a>
-            <a href="nintendo.php">Nintendo</a>
-          </div>
-      </div>
-      <a href="consoles.php">Consoles</a>
-      <a href="#">Events</a>
-    </nav>
-
-    <div class="landing">
-      <a href="events.php"><img src="assets/images/promo.png" alt="Cyber Monday Sale"></a>
+<body>
+  <nav id="main-nav">
+      <a href="index.php"><img id="logo" src="assets/images/logo.svg" alt="Logo" height="60"></a>
+    <div id="links">
+      <a href="contact.php"><i class="fas fa-at"></i></a>
+      <a href="https://goo.gl/maps/5Gnu6bCmPon9rvB86"><i class="fas fa-map-marker-alt"></i></a>
+      <a href="checkout.php"><i class="fas fa-shopping-cart"></i></a>
     </div>
+  </nav>
 
-    <div class="content center">
-      <h1>Our First Cyber Monday "Sale"!</h1>
-      <p>Starting today we have a whopping <span style="color:#F2D637;">0% OFF</span> on all XBOX games!</p>
-      <p>But wait! There's More! We are also having a and <span style="color:#F2D637;">Buy 1 Get 1 For The SAME Price</span> event for all Nintendo games!</p>
-      <a class="button" href="xbox.php">Xbox 0% Off Sale</a>
-      <a class="button" href="nintendo.php">Ninteno B1G1FTSP Event</a>
-      <br>
-      <br>
+  <nav class="nav-container">
+    <div id="content-nav">
+      <a href="index.php">Home</a>
+      <div class="dropdown">
+        <a class="dropbtn">Games <i class="fa fa-caret-down" style="font-size: 16px;"></i></a>
+        <div class="dropdown-content">
+          <a href="playstation.php">Playstation</a>
+          <a href="xbox.php">Xbox</a>
+          <a href="nintendo.php">Nintendo</a>
+        </div>
     </div>
-
-    <h1 class="center">New Games</h1>
+    <a href="consoles.php">Consoles</a>
+    <a href="#">Events</a>
+  </nav>
 
     <div class="contentslider">
-    <h2>PlayStation 4</h2>
+    <h2>Nintendo Switch</h2>
 
     <div class=x-scroller>
       <form action="<?=$_SERVER['PHP_SELF']?>" method="post">
@@ -68,7 +57,34 @@
       // print items from the catalog for selection
       foreach ($CATALOG as $k => $v)
       {
-        if ($v['type'] === "PS4games") {
+        if ($v['type'] === "NSgames") {
+          echo "<span class='article'>";
+          echo "<img class='productImage' src='assets/images/" . $v['image'] . "' alt='Product Image'><br />";
+          echo "<b class='productTitle'>" . $v['desc'] . "</b><br />";
+          echo "<hr>";
+          echo "<p class='productPrice'>$" . $CATALOG[$k]['price'] . "</p>";
+          echo "<p class='productQuantity'>Quantity: ";
+          echo "<input size=1 type=text name=\"a_qty[" . $k . "]\">";
+          echo "<input class='productSubmit' type='submit' name='add' value='Add'></p>";
+          echo "</span>";
+        }
+      }
+      ?>
+    </form>
+    </div>
+    </div>
+
+
+    <div class="contentslider">
+    <h2>Nintendo 64</h2>
+
+    <div class=x-scroller>
+      <form action="<?=$_SERVER['PHP_SELF']?>" method="post">
+      <?php
+      // print items from the catalog for selection
+      foreach ($CATALOG as $k => $v)
+      {
+        if ($v['type'] === "N64games") {
           echo "<span class='article'>";
           echo "<img class='productImage' src='assets/images/" . $v['image'] . "' alt='Product Image'><br />";
           echo "<b class='productTitle'>" . $v['desc'] . "</b><br />";
@@ -84,32 +100,5 @@
       </form>
     </div>
     </div>
-
-    <div class="contentslider">
-    <h2>Xbox One</h2>
-
-    <div class=x-scroller>
-      <form action="<?=$_SERVER['PHP_SELF']?>" method="post">
-      <?php
-      // print items from the catalog for selection
-      foreach ($CATALOG as $k => $v)
-      {
-        if ($v['type'] === "videogames") {
-          echo "<span class='article'>";
-          echo "<img class='productImage' src='assets/images/" . $v['image'] . "' alt='Product Image'><br />";
-          echo "<b class='productTitle'>" . $v['desc'] . "</b><br />";
-          echo "<hr>";
-          echo "<p class='productPrice'>$" . $CATALOG[$k]['price'] . "</p>";
-          echo "<p class='productQuantity'>Quantity: ";
-          echo "<input size=1 type=text name=\"a_qty[" . $k . "]\">";
-          echo "<input class='productSubmit' type='submit' name='add' value='Add'></p>";
-          echo "</span>";
-        }
-      }
-      ?>
-      </form>
-    </div>
-    </div>
-
   </body>
 </html>
